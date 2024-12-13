9fb80a5cec6be22e7b791cfc6e7e99dad273258a
CarloDettori <dettoric07@gmail.com>
2024-12-13 18:25:27 +0100

    need to change the cardController function

--- src/components/CardController.jsx
+++ src/components/CardController.jsx
@@ -1,13 +1,14 @@
 "use strict"
 import ButtonController from "./ButtonController";
-
+import posts from "../data/posts";
+//creare una funzione che mentre itera sugli elementi dell'array seleziona (filter) quelli true e utilizza i parametri di ogni oggetto per andare a costruire una lista (map) composta da card con all'interno i valori dei paramenti di ogni post
 function CardController(title, text) {
     return (
         <div className="card">
             <img src="https://picsum.photos/600/400" alt="600x400img" />
             <div id="info">
                 <h3>Titolo del post</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias iure nesciunt corporis pariatur architecto quo et nam ex perferendis, minus id velit facere quis voluptatem explicabo cum facilis, incidunt officia.</p>
+                <p>po</p>
                 <ButtonController />
             </div>
         </div >
