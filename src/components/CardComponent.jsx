a71e811751390745a5f909463328b2d9a95d5a3e
CarloDettori <dettoric07@gmail.com>
2024-12-16 17:14:49 +0100

    correzione con paolo

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -2,30 +2,35 @@
 import ButtonComponent from "./ButtonComponent";
 import PostSelectorComponent from "./PostSelectorComponent";
 
-function CardComponent(props) {
-    let template = [];
-    truePosts.map((element, index) => {
-        let postTags = [];
-        element.tags.map((element) => {
-            postTags.push(<li className="post-tags" id={index}>{element}</li>);
-        });
-        //console.log(postTags)
-        template.push(<div id={index} className="card">
+function CardComponent({ element }) {
+
+    // let template = [];
+    // props.posts.map((element, index) => {
+    //     // let postTags = [];
+
+    //     //console.log(postTags)
+    //     // template.push()
+    // })
+    //console.log(template)
+    return (
+        <div className="card" >
             <img src={element.image} alt="600x400img" />
             <div className="info">
                 <div className="d-flex">
                     <h3>{element.title}</h3>
                     <ul className="d-flex">
-                        {postTags}
+                        {element.tags.map((tag) => {
+                            <li className="post-tags" key={`tags-${tag}`}>
+                                {tag}
+                            </li>
+                        })}
                     </ul>
                 </div>
                 <p>{element.content}</p>
                 <ButtonComponent />
             </div>
-        </div >)
-    })
-    //console.log(template)
-    return template
+        </div>
+    )
 }
 
 export default CardComponent;
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,16 +2,39 @@
 
 import CardComponent from "./CardComponent";
 import posts from "../data/posts";
+
+
+const bestPost = {
+    id: 2,
+    title: "Carlo lorem ipsum",
+    image: 'https://picsum.photos/600/400',
+    content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "css"],
+};
+
 function MainComponent() {
     const postList = [...posts];
-    const truePosts = postList.filter((published) => published.state == "true");
+    console.log(postList);
+
+    const truePosts = postList.filter((element) => element.published === true);
+    console.log(truePosts);
+
+
     return (
         <>
             <main>
                 <div className="d-flex flex-wrap">
-                    <CardComponent posts={truePosts} />
+                    {truePosts.map((post) => (
+                        <CardComponent key={`card-${post.id}`} element={post} />
+                    ))}
+
                 </div>
             </main>
+
+            <aside>
+                <CardComponent key={`best-card-${bestPost.id}`} element={bestPost} />
+            </aside>
         </>
     )
 }
